1087235306b014ab069a8382bcf1bafe890d82ed
HABIMANA Jean de Dieu <32220586+Philos250@users.noreply.github.com>
2025-06-06 19:53:54 +0200

    Add .next/static/chunks/src_app_page_tsx_de5bbf22._.js 1749232433440

---
+++ .next/static/chunks/src_app_page_tsx_de5bbf22._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_next_f5074bf6._.js",
+  "static/chunks/src_77b85d72._.js"
+],
+    source: "dynamic"
+});
